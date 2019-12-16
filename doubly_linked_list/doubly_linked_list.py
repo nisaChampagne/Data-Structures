9567355d3eb1ab6d37a070ac9d305ba9f29627b9
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-12-16 13:34:22 -0500

    add to tail working

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -74,13 +74,25 @@ class DoublyLinkedList: # dont have to have this
     def remove_from_head(self):
         pass
 
-    """Wraps the given value in a ListNode and inserts it 
-    as the new tail of the list. Don't forget to handle 
+    """Wraps the given value in a ListNode and inserts it
+    as the new tail of the list. Don't forget to handle
     the old tail node's next pointer accordingly."""
     def add_to_tail(self, value):
-        pass
+        new_node = ListNode(value, None, None)
+        self.length += 1
+        ##if list is empty
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            #replace head; new_node = self.head
+            # next and prev are the pointers and the bucket
+            # putting a ref to our new node in the bucket
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
 
-    """Removes the List's current tail node, making the 
+    """Removes the List's current tail node, making the
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
     def remove_from_tail(self):
