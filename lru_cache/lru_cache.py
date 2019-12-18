46a03a4fe3f97054f795c3cf155e71f04248aa09
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-12-18 15:53:07 -0500

    lru comments

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -14,6 +14,7 @@ class LRUCache:
 
         self.limit = limit
         self.size = 0
+        # dict() order can change so combo with DLL helps with that
         self.storage = dict() # quickly grab key from any part of list
         self.order = DoublyLinkedList() # easier to navigate versus SLL
 
@@ -38,9 +39,11 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
+        #referencing a value
         if key in self.storage:
             node = self.storage[key]
-            self.order.move_to_end(node)
+            self.order.move_to_front(node)
+            #node.value[0] is the key, so [1] would be the value
             return node.value[1]
         else:
             return None
@@ -64,19 +67,24 @@ class LRUCache:
     want to overwrite the old value associated with the key with
     the newly-specified value.
     """
+    #set first so we have something to get
     def set(self, key, value):
+        # need to access the key to remove it later
         if key in self.storage:
             node = self.storage[key]
             node.value = (key, value)
-            self.order.move_to_end(node)
+            self.order.move_to_front(node)
             return
         else:
             if self.size == self.limit:
-                del self.storage[self.order.head.value[0]]
-                self.order.remove_from_head()
+                #shouldnt be greater than capacity
+                del self.storage[self.order.tail.value[0]]
+                self.order.remove_from_tail() #LRU
                 self.size -= 1
-        self.order.add_to_tail((key, value))
-        self.storage[key] = self.order.tail
+
+        #defining head as most recent and tail as oldest
+        self.order.add_to_head((key, value)) #node becomes MRU
+        self.storage[key] = self.order.head #the node
         self.size += 1
 
         #OR
@@ -88,3 +96,11 @@ class LRUCache:
         #         self.cache.popitem(last=False)
         # self.cache[key] = value
 
+list1 = LRUCache()
+
+list1.set("item1", "12")
+list1.set("item2", "11")
+list1.set("item3", "10")
+list1.get("item1")
+print(list1.get("item1"))
+print(list1.get("item2"))
